97e12c4ff0838bdf72eda66968436034076c6247
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2022-05-25 12:54:33 +0800

    initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_material_banner_playground/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_material_banner_playground
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomePage(title: 'Material Banner Playground'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: const Center(),
+    );
+  }
+}
